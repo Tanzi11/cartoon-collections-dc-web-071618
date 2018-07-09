fd9e1541bad16f0d4a7e7d350d27bfcc804e5683
Tanzi11 <tanzima.c@fellows.thedoschool.org>
2018-07-09 16:56:55 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,6 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves = ["Dopey", "Grumpy", "Bashful"]
+  puts dwarves.split(" ")
 end
 
 def summon_captain_planet# code an argument here
